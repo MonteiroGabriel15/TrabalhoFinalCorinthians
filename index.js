1bfed8cf94d697336a70aa0f77cf73115fdb585f
gabriel monteiro <gfonseca3117@gmai.com>
2022-11-30 23:27:41 -0300

    trabalho feito

---
+++ index.js
@@ -0,0 +1,7 @@
+const corinthians = () => {
+    window.open( "https://www.corinthians.com.br", "_blank"
+    )
+}
+
+const evento = document.querySelector("button")
+evento.addEventListener("click", corinthians)
